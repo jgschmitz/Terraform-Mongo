cab41dd0b2b9f60229e7a44d82eaa2a4387a7974
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2020-05-06 08:10:50 -0500

    Create whitelisting.tf
    we’ve improved how the Project IP Whitelist works and added support for AWS Security Groups. Previously, the entire Project IP Whitelist was defined in a single Terraform resource. Here’s an example:

---
+++ whitelisting.tf
@@ -0,0 +1,20 @@
+resource "mongodbatlas_project_ip_whitelist" "test" {
+    project_id = <PROJECT-ID>
+
+    whitelist {
+      cidr_block = "1.2.3.4/32"
+      comment    = "cidr block for tf acc testing"
+    }
+    whitelist {
+      ip_address = "2.3.4.5"
+      comment    = "ip address for tf acc testing"
+    }
+    whitelist {
+      cidr_block = "3.4.5.6/32"
+      comment    = "cidr block for tf acc testing"
+    }
+    whitelist {
+      ip_address = "4.5.6.7"
+      comment    = "ip address for tf acc testing"
+    }
+ }
