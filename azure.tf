058bac50dffac4ee7402f456a31b88026585e062
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2020-05-13 12:11:06 -0500

    Create azure.tf

---
+++ azure.tf
@@ -0,0 +1,16 @@
+resource "mongodbatlas_cluster" "test" {
+  project_id   = "<YOUR-PROJECT-ID>"
+  name         = "test"
+  num_shards   = 1
+
+  replication_factor           = 3
+  provider_backup_enabled      = true
+  auto_scaling_disk_gb_enabled = true
+  mongo_db_major_version       = "4.0"
+
+  //Provider Settings "block"
+  provider_name               = "AZURE"
+  provider_disk_type_name     = "P6"
+  provider_instance_size_name = "M30"
+  provider_region_name        = "US_EAST_2"
+}
