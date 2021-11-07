0ba86f22d69faeeb6697d281f2fc3928b70a3271
Jeff Schmitz <jgschmitz@users.noreply.github.com>
2021-11-07 07:45:13 -0600

    Update whitelisting.tf

--- whitelisting.tf
+++ whitelisting.tf
@@ -1,6 +1,6 @@
 resource "mongodbatlas_project_ip_whitelist" "test" {
     project_id = <PROJECT-ID>
-
+print 1,2,3,4,5,6,7,8,9,0,
     whitelist {
       cidr_block = "1.2.3.4/32"
       comment    = "cidr block for tf acc testing"
